010e2ff81be14ba44c44d6d10d38f8c2c9e71be8
montyD <billy-da-fish-3rd@hotmail.com>
2016-07-02 14:20:42 +0100

    pulling through packages dynamically to car page

--- mysite/code/CarPage.php
+++ mysite/code/CarPage.php
@@ -1,75 +1,74 @@
 <?php
 
-class CarPage extends Page {
+class CarPage extends Page
+{
+    private static $db = array(
+        'Name' => 'Varchar',
+        'mainDescription' => 'HTMLText',
+        'tagLine' => 'Text',
+    );
 
-	private static $db = array(
-		"Name" => "Varchar",
-		"mainDescription" => "HTMLText",
-		"tagLine" => "Text"
-	);
+    private static $has_one = array(
+      'largeImage' => 'Image',
+        'mediumImage' => 'Image',
+        'smallImage' => 'Image',
+        'squareImage' => 'Image',
+    );
 
-	private static $has_one = array(
-	  "largeImage" => "Image",
-		"mediumImage" => "Image",
-		"smallImage" => "Image",
-		"squareImage" => "Image"
-	);
+    private static $has_many = array(
+        'FeatureImages' => 'FeatureImage',
+    );
 
-	private static $has_many = array(
-		"FeatureImages" => "FeatureImage"
-	);
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeFieldFromTab('Root.Content.Main', 'Content');
 
-	public function getCMSFields() {
+        $fields->addFieldToTab('Root.Main', TextField::create('Name', 'Name'), 'Metadata');
 
-		$fields = parent::getCMSFields();
-		$fields->removeFieldFromTab("Root.Content.Main", "Content");
+        $fields->addFieldToTab('Root.Main', TextField::create('tagLine', 'Tag Line'), 'Metadata');
 
-		$fields->addFieldToTab("Root.Main", TextField::create("Name", "Name"), "Metadata");
+        $fields->addFieldToTab('Root.Main', HtmlEditorField::create('mainDescription', 'Description'), 'Metadata');
 
-		$fields->addFieldToTab("Root.Main", TextField::create("tagLine", "Tag Line"), "Metadata");
+        $fields->addFieldToTab('Root.Images', UploadField::create('largeImage', 'Large image'));
+        $fields->addFieldToTab('Root.Images', UploadField::create('mediumImage', 'Medium image'));
+        $fields->addFieldToTab('Root.Images', UploadField::create('smallImage', 'Small image'));
+        $fields->addFieldToTab('Root.Main', UploadField::create('squareImage', 'Square image'), 'mainDescription');
 
-		$fields->addFieldToTab("Root.Main", HtmlEditorField::create("mainDescription", "Description"), "Metadata");
+        $fields->addFieldToTab('Root.Images', GridField::create(
+            'FeatureImages',
+            'Images for slider',
+            $this->FeatureImages(),
+            GridFieldConfig_RecordEditor::create()
+        ));
 
-		$fields->addFieldToTab("Root.Images", UploadField::create("largeImage", "Large image"));
-		$fields->addFieldToTab("Root.Images", UploadField::create("mediumImage", "Medium image"));
-		$fields->addFieldToTab("Root.Images", UploadField::create("smallImage", "Small image"));
-		$fields->addFieldToTab("Root.Main", UploadField::create("squareImage", "Square image"), "mainDescription");
-
-		$fields->addFieldToTab("Root.Images", GridField::create(
-			"FeatureImages",
-			"Images for slider",
-			$this->FeatureImages(),
-			GridFieldConfig_RecordEditor::create()
-		));
-
-		return $fields;
-
-	}
-
+        return $fields;
+    }
 }
 
-class CarPage_Controller extends Page_Controller {
+class CarPage_Controller extends Page_Controller
+{
+    private static $allowed_actions = array(
+    );
 
-
-	private static $allowed_actions = array (
-	);
-
-	public function getPackages() {
-		if($this->Name == 'Frannie') {
-			$packages = Package::get()->filter(array(
-                 'Frannie' => true
+    public function getPackages()
+    {
+        if ($this->Name == 'Frannie') {
+            $packages = Package::get()->filter(array(
+                 'Frannie' => true,
                ));
-		} elseif ($this->Name == 'Frank') {
-			$packages = Package::get()->filter(array(
-                 'Frank' => true
+        } elseif ($this->Name == 'Frank') {
+            $packages = Package::get()->filter(array(
+                 'Frank' => true,
                ));
-		} else {
-			return false;
-		}
-		return $packages->count() ? $packages : false;
-	}
+        } else {
+            return false;
+        }
+        return $packages->count() ? $packages : false;
+    }
 
-	public function init() {
-		parent::init();
-	}
+    public function init()
+    {
+        parent::init();
+    }
 }
--- themes/dublove/templates/Layout/CarPage.ss
+++ themes/dublove/templates/Layout/CarPage.ss
@@ -32,6 +32,7 @@
   <section>
     <% include featureImageViewer %>
   </section>
+  <% if getPackages() %>
   <section class="emphasis">
     <div class="flowerGroup rightA topA up animate">
       <i class="icon-Hibiscus-flip-p one animateScale"></i>
@@ -39,22 +40,32 @@
       <i class="icon-Hibiscus-flip-p v-small three animateScale"></i>
     </div>
     <h2>Packages</h2>
-    <% loop $Packages %>
+    <% loop $getPackages() %>
       <article class="innerModal $EvenOdd">
         <div class="imageContainer">
-          <img class="b-lazy" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="$Photo.SetWidth(700).URL" alt="$Name" />
+          <% if $Top.Name == 'Frank' %>
+            <img class="b-lazy" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="$FrankPhoto.SetWidth(700).URL" alt="$Name" />
+          <% else %>
+            <img class="b-lazy" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="$FranniePhoto.SetWidth(700).URL" alt="$Name" />
+          <% end_if %>
           <div class="title">
             <h3 class="white">$Name</h3>
           </div>
         </div>
         <div class="content">
-          <img class="boxed b-lazy breakMedium break" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="$Photo.SetWidth(700).URL" alt="$Name" />
-          $Description
+          <% if $Top.Name == 'Frank' %>
+            <img class="boxed b-lazy breakMedium break" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="$FrankPhoto.SetWidth(700).URL" alt="$Name" />
+            $FrankDescription
+          <% else %>
+            <img class="boxed b-lazy breakMedium break" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-src="$FranniePhoto.SetWidth(700).URL" alt="$Name" />
+            $FrannieDescription
+          <% end_if %>
           <a class="likeButton subtle" href="#" title="Book now">Book now</a>
         </div>
       </article>
     <% end_loop %>
   </section>
+  <% end_if %>
   <section class="last">
       <p class="strongText">Not found what you're looking for? Have a look at our other packages:<a class="likeButton small" href="packages" title="Conctact us!">Packages</a></p>
   </section>
